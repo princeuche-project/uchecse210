86c2999c5e0b259116373972b3de0b7f5b8e422c
Uchenna Godstime <princeuche08@gmail.com>
2023-01-15 20:28:37 +0400

    finished prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,47 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        // How to calculate the grade percentage in C#
+
+        Console.WriteLine("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int percent = int.Parse(grade);
+
+        string scores = "";
+
+        if (percent >= 90)
+        {
+            scores = "A";
+        }
+        else if (percent >= 80)
+        {
+            scores = "B";
+        }
+        else if (percent >= 70)
+        {
+            scores = "C";
+        }
+        else if (percent >= 60)
+        {
+            scores = "D";
+        }
+        else
+        {
+            scores = "F";
+        }
+        Console.WriteLine($"Your grade is: {scores}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("You Passed ");
+        }
+        else
+        {
+            Console.WriteLine("You failed, Please try again. ");
+        }
+
+
+
     }
 }
