cd1f2e735c81c268652af8046d3b96fb78c9fbc3
Uchenna Godstime <princeuche08@gmail.com>
2023-01-15 20:30:38 +0400

    fifnishe learning02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -2,8 +2,32 @@ using System;
 
 class Program
 {
+   // private const string V = "2021";
+   // private const string V1 = "Computer Engineering";
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Console.WriteLine("Hello Learning03 World!");
+
+        Job job1 = new Job();
+        job1._jobTitle = "Web Developer";
+        job1._company = "Empower Energy Solution";
+        job1._startYear = 2022;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Web Developer";
+        job2._company = "Preferred Guard Service";
+        job2._startYear = 2020;
+        job2._endYear = 2021;
+
+        Resume myResume = new Resume();
+        myResume._name = "Uchenna Godstime";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+
+
     }
 }
