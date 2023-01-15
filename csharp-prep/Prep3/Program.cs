dff8d6043f5b2f2896c84bb2756712f67935e71a
Uchenna Godstime <princeuche08@gmail.com>
2023-01-15 20:28:57 +0400

    finished prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,33 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("What is the magic number? ");
+        int magicNumber = int.Parse(Console.ReadLine());
+
+        // To allow the computer to generate a Random number
+        // Random randomGenerator = new Random();
+        //int magicNumber = randomGenerator.Next(1, 101);
+
+
+        int guess = -1;
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your Guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Guess higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Guess lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it right! ");
+            }
+
+        }
     }
 }
