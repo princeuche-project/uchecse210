64b52b330f7cd975fd42c4222849eaf8b5ceb236
Uchenna Godstime <princeuche08@gmail.com>
2023-02-08 02:13:16 +0400

    Added changes

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,14 +4,14 @@ class Program
 {
     static void Main()
     {
-        // Create a new scripture
+        // To Create a new scripture
         Scripture scripture = new Scripture("Alma 7:23", "And now I would that ye should be ahumble, and be bsubmissive and gentle; easy to be entreated; full of patience and long-suffering; being temperate in all things; being diligent in keeping the commandments of God at all times; asking for whatsoever things ye stand in need, both spiritual and temporal; always returning thanks unto God for whatsoever things ye do receive.");
 
-        // Clear the console screen and display the complete scripture
+        //Here is to clear the console screen and display the complete scripture
         Console.Clear();
         scripture.DisplayScripture();
 
-        // Prompt the user to press enter or type quit
+        // To Prompt the user to press enter or type quit
         while (true)
         {
             Console.WriteLine("Press enter to hide a word or type quit to exit:");
@@ -24,12 +24,12 @@ class Program
             }
             else
             {
-                // Clear the console screen and hide a random word in the scripture
+                //Here is to clear the console screen and hide a random word in the scripture
                 Console.Clear();
                 scripture.HideWord();
                 scripture.DisplayScripture();
 
-                // Check if all words have been hidden
+                // Checking if all words have been hidden
                 if (scripture.AllWordsHidden())
                 {
                     Console.WriteLine("Congratulations, you have memorized the scripture!");
