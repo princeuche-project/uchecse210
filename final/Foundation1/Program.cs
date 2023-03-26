fceff056f8f59a27123091a3b543c869f4c129d9
Uchenna Godstime <princeuche08@gmail.com>
2023-03-27 02:09:51 +0400

    Made some changes

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -10,18 +10,18 @@ namespace VideoComments
         {
             List<Video> videos = new List<Video>();
 
-            Video v1 = new Video("Introduction to C#", "John Smith", 300);
+            Video v1 = new Video("Introduction to C#", "Uchenna Godstime", 300);
             v1.Comments.Add(new Comment("Alice", "Great video!"));
             v1.Comments.Add(new Comment("Bob", "Thanks for the tutorial!"));
             v1.Comments.Add(new Comment("Charlie", "Very helpful, thanks!"));
             videos.Add(v1);
 
-            Video v2 = new Video("C# OOP Concepts", "Jane Doe", 450);
+            Video v2 = new Video("C# OOP Concepts", "Uchenna Godstime", 450);
             v2.Comments.Add(new Comment("David", "Nice explanation of OOP!"));
             v2.Comments.Add(new Comment("Eva", "Very informative, thanks!"));
             videos.Add(v2);
 
-            Video v3 = new Video("Debugging Tips in C#", "Mike Johnson", 200);
+            Video v3 = new Video("Debugging Tips in C#", "Uchenna Godstime", 200);
             v3.Comments.Add(new Comment("Frank", "This saved me a lot of time!"));
             videos.Add(v3);
 
